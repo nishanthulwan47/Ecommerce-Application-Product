3d0638f7df3c79b3acd947d899bda161b569dfc4
Nishant Hulwan <nishanthulwan47@gmail.com>
2021-02-11 16:57:58 -0500

    test cases working

--- starter_code/src/main/java/com/example/demo/controllers/CartController.java
+++ starter_code/src/main/java/com/example/demo/controllers/CartController.java
@@ -36,12 +36,12 @@ public class CartController {
     public ResponseEntity<Cart> addTocart(@RequestBody ModifyCartRequest request) {
         User user = userRepository.findByUsername(request.getUsername());
         if (user == null) {
-            log.error("Error user not found", request.getUsername());
+            log.warn("Error user not found", request.getUsername());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
         Optional<Item> item = itemRepository.findById(request.getItemId());
         if (!item.isPresent()) {
-            log.error("Error Item not found in cart");
+            log.warn("Error Item not found in cart");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
         Cart cart = user.getCart();
